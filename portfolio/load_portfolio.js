efed88f9c8e22e77da17f6df31f15e7f730412bf
Nathan Getz <2036851@jeffcoschools.us>
2020-03-03 11:44:35 -0700

    _

--- portfolio/load_portfolio.js
+++ portfolio/load_portfolio.js
@@ -1,19 +1,12 @@
 // JavaScript Document
 
-var dir = [];
-
-var ns = ["3.7505", "3.0004", "2.2503", "1.5002", "0.7501", "0.0000"];
-
+var seq = ["3.7505", "3.0004", "2.2503", "1.5002", "0.7501", "0.0000"];
 
 for(var i = 1; i <= 32; i++) {
-	dir[i-1] = createElement(i.toString(),  ns[(i - 1) % 6]);
-	if(i < 10) {
-		dir[i-1] = createElement( 0 + i.toString(), ns[(i - 1) % 6]);
-	}
+    var p = (i < 10) ? "0" : "";
+	document.getElementById("content").innerHTML += createElement(p + i,  seq[(i - 1) % 6]);
 }
 
 function createElement(i, n) {
-	return '<img class="rellax floating-box" src="' + i + '_thumb.jpg" alt="" data-rellax-speed="' + n + '">';
+	return '<img class="rellax floating-box" src="/portfolio/all/' + i + '_thumb.jpg" alt="" data-rellax-speed="' + n + '">';
 }
-
-document.getElementById("dev").innerHTML = dir;
